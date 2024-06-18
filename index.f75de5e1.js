9dfac221137cc6c2343a0cf7bf64135ef662bc6b
Victor Skits <vityok.nice@gmail.com>
2024-06-18 14:34:20 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,6 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = table.rows;
+for (const row of rows)row.cells[row.cells.length - 1].insertAdjacentElement("beforebegin", row.cells[1].cloneNode(true));
+
+//# sourceMappingURL=index.f75de5e1.js.map
